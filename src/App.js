c7a2fddf4b69ec70b1c70fa492af610896811f78
nraff <nraff510@gmail.com>
2021-12-22 18:02:23 -0800

    useRef to handle inputs

--- src/App.js
+++ src/App.js
@@ -1,40 +1,25 @@
 import './App.css';
-import React, { useEffect, useReducer, useState } from "react";
+import React, { useEffect, useReducer, useState, useRef } from "react";
 import { FaStar } from "react-icons/fa"
 
-const initialState = {
-  message: "hi"
-}
-
-function reducer(state, action) {
-  switch(action.type) {
-    case "yell":
-      return {
-        message: `HEY! I JUST SAID ${state.message}`
-      } 
-    case "whisper":
-      return {
-        message: `excuse me, I just said ${state.message}`
-      }
-    default:
-      return {
-        message: "hi"
-      }
-  }
-}
-
-
 function App() {
-  const [state, dispatch] = useReducer(
-    reducer,
-    initialState
-  )
+  const sound = useRef()
+  const color = useRef()
+
+  const submit = (e) => {
+    e.preventDefault();
+    const soundVal = sound.current.value
+    const colorVal = color.current.value
+    alert(`${soundVal} sounds like ${colorVal}`)
+    sound.current.value = ""
+    color.current.value = ""
+  }
   return (
-    <>
-      <p>Message: {state.message}</p>
-      <button onClick={() => dispatch({type: "yell"})}>yell</button>
-      <button onClick={() => dispatch({type: "whisper"})}>whisper</button>
-    </>
+    <form onSubmit={submit}>
+      <input ref={sound} type="text" placeholder='Sound...'/>
+      <input ref={color} type="color" />
+      <button>ADD</button>
+    </form>
   )
 }
 
