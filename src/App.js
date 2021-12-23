2e43298fd57fa2248f7b721d232f2d44616a009a
nraff <nraff510@gmail.com>
2021-12-22 17:45:45 -0800

    working with useReducer

--- src/App.js
+++ src/App.js
@@ -1,36 +1,17 @@
 import './App.css';
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import { FaStar } from "react-icons/fa"
 
 
 
 function App() {
-  const [data, setData] = useState([])
-
-  useEffect(() => {
-    fetch(`https://api.github.com/users`)
-    .then(response => response.json())
-    .then(res => setData(res))
-  }, [])
-
-  if(data) {
-    return (
-      <div>
-        <ul>
-          {data.map(user => <li key={user.id}>{user.login}</li>)}
-        </ul>
-        <button
-          onClick={() => setData([])}
-        >
-          Remove Data
-        </button>
-      </div>
-    )
-  } else {
-    return (
-      <p>No Users</p>
-    )
-  }
+  const [number, setNumber] = useReducer(
+    (number, newNumber) => number + newNumber, 0
+  )
+  return (
+    <h1
+      onClick={() => setNumber(1)}>{number}</h1>
+  )
 }
 
 export default App;
