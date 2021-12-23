059cc8292ec80c7150e58fcaf43e6e08021628cb
nraff <nraff510@gmail.com>
2021-12-22 17:49:31 -0800

    refactor checkbox with useReducer

--- src/App.js
+++ src/App.js
@@ -5,12 +5,19 @@ import { FaStar } from "react-icons/fa"
 
 
 function App() {
-  const [number, setNumber] = useReducer(
-    (number, newNumber) => number + newNumber, 0
+  const [checked, toggle] = useReducer(
+    checked => !checked,
+    false
   )
   return (
-    <h1
-      onClick={() => setNumber(1)}>{number}</h1>
+    <>
+      <input
+        type="checkbox"
+        value={checked}
+        onChange={toggle}
+      />
+      {checked ? "checked" : "not checked"}
+    </>
   )
 }
 
